d68b8aaced497076c2173bdc3a65bc2b54784d14
Karlo Novak <karlo.novak@greyp.com>
2021-09-22 15:19:36 +0200

    implement good guess logic

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,11 +1,55 @@
 defmodule Hangman.Game do
   defstruct state: :initializing,
             turns_left: 7,
-            letters: []
+            letters: [],
+            used: MapSet.new()
 
-  def new_game() do
+  def new_game(word) do
     %Hangman.Game{
-      letters: Dictionary.random_word() |> String.codepoints()
+      letters: word |> String.codepoints()
     }
   end
+
+  def new_game() do
+    new_game(Dictionary.random_word())
+  end
+
+  def make_move(game = %Hangman.Game{state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+    accept_move(game, guess, MapSet.member?(game.used, guess))
+  end
+
+  defp accept_move(game, _guess, _already_guessed = true) do
+    Map.put(game, :state, :already_used)
+  end
+
+  defp accept_move(game, guess, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+    |> good_guess?(guess in game.letters)
+  end
+
+  defp good_guess?(game, true) do
+    game_won?(game, MapSet.subset?(MapSet.new(game.letters), game.used))
+  end
+
+  defp good_guess?(game, false) do
+    game
+  end
+
+  defp game_won?(game, true) do
+    Map.put(game, :state, :won)
+  end
+
+  defp game_won?(game, _) do
+    Map.put(game, :state, :good_guess)
+  end
+
+
+  def tally(_game)  do
+    123
+  end
+
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -1,8 +1,49 @@
-defmodule HangmanTest do
+defmodule GameTest do
   use ExUnit.Case
-  doctest Hangman
+  alias Hangman.Game
 
   test "state is initialized" do
-    assert %Hangman.Game{state: :initializing} = Hangman.new_game()
+    game = Game.new_game()
+
+    assert game.state == :initializing
+    assert game.turns_left == 7
+    assert length(game.letters) > 0
+    assert game.letters |> List.to_string |> String.downcase == game.letters |> List.to_string
   end
+
+  test "game state is unchanged on making a move if game is :won or :lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:state, state)
+      assert {^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  test "already guessed check" do
+    game = Game.new_game()
+
+    game = Game.make_move(game, "x")
+    assert game.state != :already_used
+
+    game = Game.make_move(game, "x")
+    assert game.state == :already_used
+  end
+
+  test "good guess" do
+    game = Game.new_game("doodle")
+    game = Game.make_move(game, "d")
+    assert game.state == :good_guess
+  end
+
+  test "victory if all good guessed" do
+    game = Game.new_game("doodle")
+    game = Game.make_move(game, "d")
+    assert game.state == :good_guess
+    game = Game.make_move(game, "o")
+    assert game.state == :good_guess
+    game = Game.make_move(game, "l")
+    assert game.state == :good_guess
+    game = Game.make_move(game, "e")
+    assert game.state == :won
+  end
+
 end
